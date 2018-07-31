35c485a06c99c70922351f290d3a0c8ce8f57c7e
Vince van Oosten <techhazard@codeforyouand.me>
2018-07-31 17:39:31 +0200

    add test dir

---
+++ flutter_frontend/test/app_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_travis_sample/main.dart';
+
+void main() {
+  testWidgets('app should work', (tester) async {
+    await tester.pumpWidget(new MyApp());
+    expect(find.text('Hello, World!'), findsOneWidget);
+  });
+}

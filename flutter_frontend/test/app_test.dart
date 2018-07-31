2d97364699d1463327f756c98ec79ecb74dfa60f
Vince van Oosten <techhazard@codeforyouand.me>
2018-07-31 17:42:42 +0200

    fix package name

--- flutter_frontend/test/app_test.dart
+++ flutter_frontend/test/app_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_travis_sample/main.dart';
+import 'package:flutter_frontend/main.dart';
 
 void main() {
   testWidgets('app should work', (tester) async {

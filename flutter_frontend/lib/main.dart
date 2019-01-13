660225a797b2a4e28c32ee4772097a34d870da5b
Vince van Oosten <techhazard@codeforyouand.me>
2019-01-13 13:46:48 +0100

    remove flutter stuff

--- flutter_frontend/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-main() {
-  runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  build(_) => new Text('Hello, World!');
-}
--- flutter_frontend/test/app_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_frontend/main.dart';
-
-void main() {
-  testWidgets('app should work', (tester) async {
-    await tester.pumpWidget(new MyApp());
-    expect(find.text('Hello, World!'), findsOneWidget);
-  });
-}
